569c09197981fe476063b3ec39973e94c60721ba
Quentin Canu <qcanu17@gmail.com>
2025-11-02 19:53:13 +0100

    -

---
+++ Cours/rng_gen.py
@@ -0,0 +1,10 @@
+#!/Library/Frameworks/Python.framework/Versions/3.8/bin/python3
+
+import random as rd
+import argparse
+
+def main():
+    print(rd.randrange(0,10))
+
+if __name__ == "__main__":
+    main()
